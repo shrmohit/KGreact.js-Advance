ed433ce6e39d3d1ce982599032fb21657a9787d2
Mohit Sharma <mohitsharma7258@gmail.com>
2025-02-14 09:14:01 +0530

    start bootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Slidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Slidebar></Slidebar>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
