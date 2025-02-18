34cb53b573e3c236ad508962a336a11bfc544bbc
Mohit Sharma <mohitsharma7258@gmail.com>
2025-02-18 11:11:35 +0530

    router intro

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Createpost from "./components/Createpost.jsx";
 
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+//router
+// createBrowserRouterit is to create router
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  {
+    path: "/createpost",
+    element: <Createpost />,
+  },
+]); // this is default path
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} /> // Rp is import as a components and pass
+    router to routing
+  </StrictMode>
+);
